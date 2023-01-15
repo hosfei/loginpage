330035511ef58c81f273d183d9af05e4ab9e1308
hosfei <112815233+hosfei@users.noreply.github.com>
2023-01-15 23:51:24 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,38 +1,37 @@
-let userNameInput = document.getElementById("userNameInput")
-let passwordInput = document.getElementById("passwordInput")
-let loginBtn = document.getElementById("loginBtn")
-let signUpBtn = document.getElementById("signUpBtn")
-let newUserName = document.getElementById("newUserName")
-let newPassword = document.getElementById("newPassword")
-
-let demo = document.getElementById("demo")
-if (localStorage.getItem("users")) {
-} else {
-  let users = [
-    { id: 0, userName: "janne", password: "test" },
-    { id: 1, userName: "hossein", password: "feili" },
-  ]
-  localStorage.setItem("users", JSON.stringify(users))
-}
-
-loginBtn.addEventListener("click", () => {
-  let users = JSON.parse(localStorage.getItem("users"))
-  let user = users.find(
-    (user) => user.userName === userNameInput.value && user.password === passwordInput.value
-  )
-  if (user) {
-    let loggedInUser = userNameInput.value
-    localStorage.setItem("loggedInUser", loggedInUser)
-    demo.innerHTML = `Logged in as ${loggedInUser}`
-    let logoutBtn = document.createElement("button")
-    logoutBtn.innerHTML = "Logga ut"
-    logoutBtn.addEventListener("click", () => {
-      localStorage.removeItem("loggedInUser")
-      demo.innerHTML = ""
-      logoutBtn.remove()
+window.onload = function() {
+    let userNameInput = document.getElementById("userNameInput")
+    let passwordInput = document.getElementById("passwordInput")
+    let loginBtn = document.getElementById("loginBtn")
+    let demo = document.getElementById("demo")
+  
+    let users = [
+      { id: 0, userName: "janne", password: "test" },
+      { id: 1, userName: "hossein", password: "feili" },
+    ]
+    localStorage.setItem("users", JSON.stringify(users))
+  
+    loginBtn.addEventListener("click", () => {
+      let users = JSON.parse(localStorage.getItem("users"))
+      let user = users.find(
+        (user) =>
+          user.userName === userNameInput.value &&
+          user.password === passwordInput.value
+      )
+      if (user) {
+        let loggedInUser = userNameInput.value
+        localStorage.setItem("loggedInUser", loggedInUser)
+        demo.innerHTML = `Logged in as ${loggedInUser}`
+        let logoutBtn = document.createElement("button")
+        logoutBtn.innerHTML = "Logga ut"
+        logoutBtn.addEventListener("click", () => {
+          localStorage.removeItem("loggedInUser")
+          demo.innerHTML = ""
+          logoutBtn.remove()
+        })
+        demo.appendChild(logoutBtn)
+      } else {
+        demo.innerHTML = ""
+      }
     })
-    demo.appendChild(logoutBtn)
-  } else {
-   
   }
-});
+  
